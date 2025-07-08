6abb9d80801876258703e2821366ad4f28e11395
BlessmanJay <blessmanjay@gmail.com>
2025-07-08 14:55:12 +0100

    Add WelcomeMessage.jsx file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from "./WelcomeMessage";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -8,6 +9,11 @@ function App() {
 
   return (
     <>
+      <div>
+        <h1>Hello everyone, I am learning React at ALX!</h1>
+        <p>This is a simple JSX component.</p>
+        <p>I am learning about JSX!</p>
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
