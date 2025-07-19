8c1bcbfcef93b42a6b365029426affb5e3ad29b8
BlessmanJay <blessmanjay@gmail.com>
2025-07-19 08:21:12 +0100

    Add New Directory

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+// Footer.jsx
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#333",
+        color: "#fff",
+        textAlign: "center",
+        padding: "15px",
+        position: "fixed",
+        width: "100%",
+        bottom: "0",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,24 @@
+// MainContent.jsx
+import React from "react";
+
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#e0f7fa",
+      }}
+    >
+      <p
+        style={{
+          color: "rgb(50, 100, 80 )",
+          fontSize: "1.5rem",
+        }}
+      >
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
