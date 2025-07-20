746e71e9c8cecd9d31ed63baae3077c00b2acd9a
BlessmanJay <blessmanjay@gmail.com>
2025-07-20 14:19:42 +0100

    Initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#e0f7fa",
+        minHeight: "200px",
+      }}
+    >
+      <h2 style={{ color: "#00796b" }}>Welcome to My Favorite Cities</h2>
+      <p>Here you'll find a list of the most amazing cities in the world.</p>
+    </main>
+  );
+}
+
+export default MainContent;
