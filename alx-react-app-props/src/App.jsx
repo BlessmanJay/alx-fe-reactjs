f9bc6f293935908b88a0c239e488f98db22c6383
Bless John <166983244+BlessmanJay@users.noreply.github.com>
2025-07-23 07:58:57 +0100

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,6 +4,7 @@ import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import UserContext from "./components/Usercontext";
 import Footer from "./components/Footer";
 import WelcomeMessage from "./components/WelcomeMessage";
 import { useState } from "react";
@@ -15,38 +16,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
-      </div>
-      <Counter />
-      <UserProfile name="Bob" age={30} bio="Enjoys coding and coffee" />
-      <Header />
-      <MainContent />
-      <Footer />
-      <WelcomeMessage />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Hello, world!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <UserContext.Provider value={userData}>
+     <ProfilePage/>
+     </UserContext.Provider>
   );
 }
 
