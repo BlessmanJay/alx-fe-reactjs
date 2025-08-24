b24c4160eb639cab452f11dd175c90826fba9452
BlessmanJay <blessmanjay@gmail.com>
2025-08-24 15:38:42 +0100

    Add new React project: react-todo

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,43 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+});
+
+test("adds a new todo", () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText("Enter todo");
+  const button = screen.getByText("Add");
+
+  fireEvent.change(input, { target: { value: "New Todo" } });
+  fireEvent.click(button);
+
+  expect(screen.getByText("New Todo")).toBeInTheDocument();
+});
+
+test("toggles a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+
+  // Initially not completed
+  expect(todo).not.toHaveStyle("text-decoration: line-through");
+
+  fireEvent.click(todo);
+
+  // After toggle
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("deletes a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  const deleteBtn = screen.getAllByText("Delete")[0];
+
+  fireEvent.click(deleteBtn);
+
+  expect(todo).not.toBeInTheDocument();
+});
